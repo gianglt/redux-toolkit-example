b9bd9c4a451b600b567a2a057c0e273df6e49abe
Giang <gianglt@gmail.com>
2022-05-10 14:21:25 +0700

    Hoc redux

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,21 +1,48 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Button, StyleSheet, TextInput } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../store';
 import { setMessage } from '../store/message';
 
 const Message = () => {
   const dispatch = useDispatch();
-  const { message } = useSelector((state: RootState) => state.message);
+  // const [text, onChangeText] = React.useState("");
+  const { message} = useSelector((state: RootState) => state.message);
+
+
+  const onChangeText = (text: string) => {
+    dispatch(setMessage(text));
+  };
 
   const handlePress = () => {
-    dispatch(setMessage('Message from Component'));
+    dispatch(setMessage("Reset text"));
   };
+
+  // useEffect(() => {
+  
+  //   return () => {
+      
+  //   }
+  // }, [])
+  
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{message}</Text>
-      <Button title={'Set Message'} onPress={handlePress} />
+      <View style={styles.row}>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => onChangeText(text)}
+          value={message}
+        />
+
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.text}>{message}</Text>
+      </View>
+      <View style={styles.row}>
+        <Button title={'Reset Message'} onPress={handlePress} />
+      </View>
+
     </View>
   );
 };
@@ -24,11 +51,21 @@ export default Message;
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
+    backgroundColor: '#bbbbff',
+    marginTop: 10,
+    marginBottom: 10,
   },
   text: {
     fontSize: 18
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    width: 200,
+    backgroundColor: '#ffffff',
+  },
+  row: {
+    flexDirection: 'row',
+    padding: 5,
   }
 });
